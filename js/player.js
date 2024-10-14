881b9e10bea06e042599148d7562f43a049a4ea7
Anna6463 <86959325+Anna6463@users.noreply.github.com>
2024-10-14 21:20:17 +0300

    Add files via upload

--- js/player.js
+++ js/player.js
@@ -3,35 +3,35 @@
 let playlist_1 = {
   number: 1,
   title: "Hip-Hop Hits",
-  coverImageUrl: "/img/playlist_1.png",
+  coverImageUrl: "img/playlist_1.png",
   info: {
     totaltrackcount: 4,
     totaltrackDurationInSecond: 733,
   },
   tracks: [
     {
-      coverImageUrl: "/img/artict_1.png",
+      coverImageUrl: "img/artict_1.png",
       artistName: "Emenem",
       title: "Rap God",
       fileUrl: "Emenem_-_Rap_God.mp3",
       isHot: false,
     },
     {
-      coverImageUrl: "/img/artict_2.png",
+      coverImageUrl: "img/artict_2.png",
       artistName: "50 cent",
       title: "50 cent - In da Club",
       fileUrl: "50 cent - In da Club.mp3",
       isHot: false,
     },
     {
-      coverImageUrl: "/img/artict_3.png",
+      coverImageUrl: "img/artict_3.png",
       artistName: "DMX - X Gon",
       title: "DMX - X Gon Give It To Ya",
       fileUrl: "DMX - X Gon Give It To Ya.mp3",
       isHot: true,
     },
     {
-      coverImageUrl: "/img/artict_4.png",
+      coverImageUrl: "img/artict_4.png",
       artistName: "Eminem feat 50 Cent, Cashis, Lloyd Banks",
       title: "You Dont Know",
       fileUrl: "Eminem feat 50 Cent, Cashis, Lloyd Banks.mp3",
@@ -43,35 +43,35 @@ let playlist_1 = {
 let playlist_2 = {
   number: 2,
   title: "Rap Hits 1990s",
-  coverImageUrl: "/img/playlist_2.png",
+  coverImageUrl: "img/playlist_2.png",
   info: {
     totaltrackcount: 4,
     totaltrackDurationInSecond: 733,
   },
   tracks: [
     {
-      coverImageUrl: "/img/artict_2-1.png",
+      coverImageUrl: "img/artict_2-1.png",
       artistName: "Public Enemy",
       title: "Fight the Power",
       fileUrl:  "Emenem_-_Rap_God.mp3",
       isHot: false,
     },
     {
-      coverImageUrl: "/img/artict_2-2.png",
+      coverImageUrl: "img/artict_2-2.png",
       artistName: "Vanila Ice",
       title: "Ice Ice Baby",
       fileUrl: "50 cent - In da Club.mp3",
       isHot: false,
     },
     {
-      coverImageUrl: "/img/artict_2-4.png",
+      coverImageUrl: "img/artict_2-4.png",
       artistName: "MC Hammer",
       title: "You Can’t Touch This",
       fileUrl: "Eminem feat 50 Cent, Cashis, Lloyd Banks.mp3",
       isHot: false,
     },
     {
-      coverImageUrl: "/img/artict_2-3.png",
+      coverImageUrl: "img/artict_2-3.png",
       artistName: "Brand Nubian",
       title: "Brand Nubian",
       fileUrl: "DMX - X Gon Give It To Ya.mp3",
